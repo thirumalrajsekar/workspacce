bb38abedb44a7f4fab634c003ec5eacc346b2037
thirumalrajsekar <thirumalraj.s@gmail.com>
2016-08-29 23:29:02 +0530

    Update Connectivity.py
    commit for sample

--- Connectivity.py
+++ Connectivity.py
@@ -1,13 +1,1 @@
-import jenkins
-import socket
-
-server = jenkins.Jenkins('http://localhost:8080', username='thirumalraj.s', password='password')
-user = server.get_whoami()
-##version = server.get_version()
-versionss = server.get_version
-print('Hello %s from Jenkins %s' % (user['fullName'], versionss))
-devices = socket.gethostname()
-listt = server.get_all_jobs()
-
-print listt
-print devices
+print "testing Jenkins"
